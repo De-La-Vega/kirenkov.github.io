edc5111902d7587466bb9503f37b99f27e2d771d
Vitaliy Kirenkov <vkirenk2@ru.ibm.com>
2017-06-03 00:51:54 +0300

    optimize img and remove contacts form

--- src/Sections/Contacts.jsx
+++ src/Sections/Contacts.jsx
@@ -20,28 +20,24 @@ export default class Contacts extends React.Component {
                     <h2 className="inverted">Contact <span>me</span></h2>
                     <h3 className="inverted">Get in touch</h3>
 
-                    <br />
-                    <br />
-
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-10 col-sm-offset-1">
-                            <div className="row">
-                                <div className="col-xs-12 col-sm-6 col-md-4 data-contacts">
-                                    <div className="item-contact">
-                                        <div className="icon"><span className="icon-contact icon-skype"></span></div>
-                                        <div className="text base-text"><strong>Skype:</strong><br />de_la_vega666</div>
-                                    </div>
-                                    <div className="item-contact">
-                                        <div className="icon"><span className="icon-contact icon-phone"></span></div>
-                                        <div className="text base-text"><strong>Phone:</strong><br />+7 (916) 961-71-95</div>
-                                    </div>
-                                    <div className="item-contact">
-                                        <div className="icon"><span className="icon-contact icon-mail-alt"></span></div>
-                                        <div className="text base-text"><strong>E-mail:</strong><br /><a href="mailto:kirenkov.vitaliy@gmail.com">kirenkov.vitaliy@gmail.com</a></div>
-                                    </div>
+                    <div className="contacts-container">
+                        <div className="row">
+                            <div className="col-xs-12 col-sm-4">
+                                <div className="item-contact">
+                                    <div className="icon"><span className="icon-contact icon-mail-alt"></span></div>
+                                    <div className="text base-text"><strong>E-mail:</strong><br /><a href="mailto:kirenkov.vitaliy@gmail.com">kirenkov.vitaliy@gmail.com</a></div>
                                 </div>
-                                <div className="col-xs-12 col-sm-6 col-md-8 data-form">
-                                    <ContactsForm />
+                            </div>
+                            <div className="col-xs-12 col-sm-4">
+                                <div className="item-contact">
+                                    <div className="icon"><span className="icon-contact icon-phone"></span></div>
+                                    <div className="text base-text"><strong>Phone:</strong><br />+7 (916) 961-71-95</div>
+                                </div>
+                            </div>
+                            <div className="col-xs-12 col-sm-4">
+                                <div className="item-contact">
+                                    <div className="icon"><span className="icon-contact icon-skype"></span></div>
+                                    <div className="text base-text"><strong>Skype:</strong><br />de_la_vega666</div>
                                 </div>
                             </div>
                         </div>
