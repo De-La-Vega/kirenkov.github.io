72515c196fedefe859366e519b49d9dc89170179
Vitaliy Kirenkov <vkirenk2@ru.ibm.com>
2019-07-19 17:47:40 +0400

    fixed footer / updadted packages / added lang

--- src/Sections/Certificates/index.tsx
+++ src/Sections/Certificates/index.tsx
@@ -8,55 +8,59 @@ export const Certificates: React.SFC = () => (
             <h2 className="text-center">My <span>Certificates</span></h2>
 
             <div className="row">
-                <div className="col-xs-6">
-                    <div className="item-certificate item-certificate__scrum">
-                        <img src="assets/images/certificates/csm.jpg" className="img-responsive" alt="" />
+                <div className="col-xs-12 col-sm-10 col-sm-offset-1">
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <div className="item-certificate item-certificate__scrum">
+                                <img src="assets/images/certificates/csm.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xs-6">
+                            <div className="item-certificate item-certificate__scrum">
+                                <img src="assets/images/certificates/acsm.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-xs-6">
-                    <div className="item-certificate item-certificate__scrum">
-                        <img src="assets/images/certificates/acsm.jpg" className="img-responsive" alt="" />
-                    </div>
-                </div>
-            </div>
 
-            <div className="row">
-                <div className="col-xs-4">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-4.jpg" className="img-responsive" alt="" />
+                    <div className="row">
+                        <div className="col-xs-4">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-4.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xs-4">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-2.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xs-4">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-7.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-xs-4">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-2.jpg" className="img-responsive" alt="" />
-                    </div>
-                </div>
-                <div className="col-xs-4">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-7.jpg" className="img-responsive" alt="" />
-                    </div>
-                </div>
-            </div>
 
-            <div className="row">
-                <div className="col-xs-6 col-lg-3">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-1.jpg" className="img-responsive" alt="" />
-                    </div>
-                </div>
-                <div className="col-xs-6 col-lg-3">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-5.jpg" className="img-responsive" alt="" />
-                    </div>
-                </div>
-                <div className="col-xs-6 col-lg-3">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-6.jpg" className="img-responsive" alt="" />
-                    </div>
-                </div>
-                <div className="col-xs-6 col-lg-3">
-                    <div className="item-certificate item-certificate__udemy">
-                        <img src="assets/images/certificates/udemy-3.jpg" className="img-responsive" alt="" />
+                    <div className="row">
+                        <div className="col-xs-6 col-lg-3">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-1.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xs-6 col-lg-3">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-5.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xs-6 col-lg-3">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-6.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-xs-6 col-lg-3">
+                            <div className="item-certificate item-certificate__udemy">
+                                <img src="assets/images/certificates/udemy-3.jpg" className="img-responsive" alt="" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
